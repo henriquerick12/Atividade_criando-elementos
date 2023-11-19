06d3656c3ace3d417bb6af81052816b4d9e8e44c
Henrique_Rick <henriquewood3@gmail.com>
2023-11-19 15:19:59 -0300

    Feito funções createElement e renderElement para criar e mostrar lista

--- script.js
+++ script.js
@@ -1,2 +1,53 @@
-import { products } from './dbProducts'
+const products = [
+  {
+    name: "Camiseta",
+    price: 29.99,
+  },
+  {
+    name: "Calça Jeans",
+    price: 59.99,
+  },
+  {
+    name: "Sapatos",
+    price: 59.99,
+  },
+  {
+    name: "Vestido",
+    price: 89.99,
+  },
+  {
+    name: "Chapéu",
+    price: 19.99,
+  },
+];
 
+const ulItem = document.querySelector("ul");
+
+const createElement = (array) => {
+  const { name, price } = array;
+
+  const liItem = document.createElement("li");
+  const h3Item = document.createElement("h3");
+  const pItem = document.createElement("p");
+  const buttonItem = document.createElement("button");
+
+  h3Item.innerHTML = name;
+  pItem.innerHTML = `R$ ${price.toFixed(2)}`;
+  buttonItem.innerHTML = "Adicionar ao carrinho";
+
+  liItem.append(h3Item, pItem, buttonItem);
+
+  return liItem;
+};
+
+const renderElement = (array) => {
+  ulItem.innerHTML = "";
+
+  array.forEach((element) => {
+    const item = createElement(element);
+
+    ulItem.appendChild(item);
+  });
+};
+
+renderElement(products);
